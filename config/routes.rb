c886ff9abf7d3f271f0650603a097b962f1ab8ed
Duc Dang-Phuc <ducdp@administrator.local.elarion.com>
2018-05-17 17:11:17 +0700

    Finish Rails Begin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
    get 'welcome/index'
-   resources :articles
+   
+
+
+resources :articles do
+  resources :comments
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
 end
