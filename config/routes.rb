8f81d8fbd244f42d4f7534cd11346307bd6a82a0
Duc Dang-Phuc <ducdp@administrator.local.elarion.com>
2018-05-17 13:57:11 +0700

    Save and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-   resource :welcome
-   resource :articles
+   get 'welcome/index'
+   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
