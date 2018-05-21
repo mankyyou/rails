89dadcb2cd85230ab1e9a9afac0c0b6d3bdd0aa2
Duc Dang-Phuc <ducdp@administrator.local.elarion.com>
2018-05-21 16:41:10 +0700

    Finish Basiclast

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,7 @@ class Article < ApplicationRecord
 	 has_many :comments, dependent: :destroy
   validates :title,
    :presence=>{:presence=>true,:message=>"Dung bo trong ma :'("},
-   :length=>{:minimum =>5,:message=>"Nho nhat la 5"}
-    validates :term,
-    :acceptance => {:acceptance=>true,:message=>"Stick vao yeu cau"}
+   :length=>{:minimum =>1,:message=>"Nho nhat la 5"}
     validates :text,
     :length=>{:maximum=>20,:message=>"Lon nhat la 20"}
 
